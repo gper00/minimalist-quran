096b3584e6c3a76d88beff7d39f758d8e7d1337f
devchampl00 <alfariziuchiha@gmail.com>
2026-04-18 02:29:15 +0800

    fix: upgrade next.js 16, fix turbopack config

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,13 @@ import withSerwistInit from "@serwist/next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  turbopack: {},
 }
 
 const withSerwist = withSerwistInit({
